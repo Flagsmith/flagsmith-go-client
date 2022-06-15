5fd3106d03b44b277b1ce15825c212704ffe10a0
Gagan Trivedi <gagandeeptrivedi47@gmail.com>
2022-06-15 14:00:38 +0530

    Add example project

---
+++ example/server.go
@@ -0,0 +1,81 @@
+package main
+
+import (
+	"encoding/json"
+	"fmt"
+	"github.com/Flagsmith/flagsmith-go-client"
+	"html/template"
+	"log"
+	"net/http"
+	"os"
+)
+
+func main() {
+	http.HandleFunc("/", RootHandler)
+
+	fmt.Printf("Starting server at port 5000\n")
+	if err := http.ListenAndServe(":5000", nil); err != nil {
+		log.Fatal(err)
+	}
+}
+
+type TemplateData struct {
+	Identifier   string
+	ShowButton   bool
+	ButtonColour string
+}
+
+func RootHandler(w http.ResponseWriter, r *http.Request) {
+	// Intialise the flagsmith client
+	client := flagsmith.NewClient(os.Getenv("FLAGSMITH_API_KEY"),
+		flagsmith.WithBaseURL("http://localhost:8000/api/v1/"))
+	q := r.URL.Query()
+
+	if q.Get("identifier") != "" {
+		identifier := q.Get("identifier")
+		var traits []*flagsmith.Trait
+		traits = nil
+
+		if q.Get("trait-key") != "" {
+			trait := flagsmith.Trait{TraitKey: q.Get("trait-key"), TraitValue: q.Get("trait-value")}
+			traits = []*flagsmith.Trait{&trait}
+		}
+
+		flags, _ := client.GetIdentityFlags(identifier, traits)
+
+		showButton, _ := flags.IsFeatureEnabled("secret_button")
+		buttonData, _ := flags.GetFeatureValue("secret_button")
+
+		// convert button data to map
+		buttonData = buttonData.(string)
+		var buttonDataMap map[string]string
+		json.Unmarshal([]byte(buttonData.(string)), &buttonDataMap)
+
+		templateData := TemplateData{
+			Identifier:   identifier,
+			ShowButton:   showButton,
+			ButtonColour: buttonDataMap["colour"],
+		}
+		t, _ := template.ParseFiles("home.html")
+		t.Execute(w, templateData)
+		return
+	}
+	flags, _ := client.GetEnvironmentFlags()
+
+	showButton, _ := flags.IsFeatureEnabled("secret_button")
+
+	buttonData, _ := flags.GetFeatureValue("secret_button")
+
+	// convert button data to map
+	buttonData = buttonData.(string)
+	var buttonDataMap map[string]string
+	json.Unmarshal([]byte(buttonData.(string)), &buttonDataMap)
+
+	templateData := TemplateData{
+		ShowButton:   showButton,
+		ButtonColour: buttonDataMap["colour"],
+	}
+
+	t, _ := template.ParseFiles("home.html")
+	t.Execute(w, templateData)
+}
