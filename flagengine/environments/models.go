e5922646016dfde69d175f318fcdb8977bfd6329
Kim Gustyr <kim.gustyr@flagsmith.com>
2024-04-05 18:27:05 +0100

    feat: Identity overrides in local evaluation mode (#121)
    * feat: Identity overrides in local evaluation mode

--- flagengine/environments/models.go
+++ flagengine/environments/models.go
@@ -1,19 +1,15 @@
 package environments
 
 import (
-	"github.com/Flagsmith/flagsmith-go-client/v3/flagengine/environments/integrations"
 	"github.com/Flagsmith/flagsmith-go-client/v3/flagengine/features"
+	"github.com/Flagsmith/flagsmith-go-client/v3/flagengine/identities"
 	"github.com/Flagsmith/flagsmith-go-client/v3/flagengine/projects"
 )
 
 type EnvironmentModel struct {
-	ID            int                           `json:"id"`
-	APIKey        string                        `json:"api_key"`
-	Project       *projects.ProjectModel        `json:"project"`
-	FeatureStates []*features.FeatureStateModel `json:"feature_states"`
-
-	AmplitudeConfig *integrations.IntegrationModel `json:"amplitude_config"`
-	SegmentConfig   *integrations.IntegrationModel `json:"segment_config"`
-	MixpanelConfig  *integrations.IntegrationModel `json:"mixpanel_config"`
-	HeapConfig      *integrations.IntegrationModel `json:"heap_config"`
+	ID                int                           `json:"id"`
+	APIKey            string                        `json:"api_key"`
+	Project           *projects.ProjectModel        `json:"project"`
+	FeatureStates     []*features.FeatureStateModel `json:"feature_states"`
+	IdentityOverrides []*identities.IdentityModel   `json:"identity_overrides"`
 }
