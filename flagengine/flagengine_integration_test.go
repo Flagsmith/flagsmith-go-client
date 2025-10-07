f353451da27cbfb3404f98698813a8d7874839e1
Gagan Trivedi <gagandeeptrivedi47@gmail.com>
2025-10-07 08:22:38 +0530

    use new integration tests

--- flagengine/flagengine_integration_test.go
+++ flagengine/flagengine_integration_test.go
@@ -3,7 +3,8 @@ package flagengine_test
 import (
 	"encoding/json"
 	"os"
-	"strconv"
+	"path/filepath"
+	"strings"
 	"testing"
 
 	"github.com/stretchr/testify/assert"
@@ -11,35 +12,45 @@ import (
 
 	"github.com/Flagsmith/flagsmith-go-client/v5/flagengine"
 	"github.com/Flagsmith/flagsmith-go-client/v5/flagengine/engine_eval"
-	"github.com/Flagsmith/flagsmith-go-client/v5/flagengine/environments"
 )
 
-const TestData = "./engine-test-data/data/environment_n9fbf9h3v4fFgH3U3ngWhb.json"
+const TestDataDir = "./engine-test-data/test_cases"
 
 func TestEngine(t *testing.T) {
 	t.Parallel()
-	var testData struct {
-		Environment environments.EnvironmentModel `json:"environment"`
-		TestCases   []struct {
-			EvaluationContext engine_eval.EngineEvaluationContext `json:"context"`
-			EvaluationResult  engine_eval.EvaluationResult        `json:"result"`
-		} `json:"test_cases"`
-	}
 
-	testSpec, err := os.ReadFile(TestData)
+	// Read all test case files from the test_cases directory
+	files, err := filepath.Glob(filepath.Join(TestDataDir, "*.json"))
 	require.NoError(t, err)
-	require.NotEmpty(t, testSpec)
+	require.NotEmpty(t, files, "No test case files found in %s", TestDataDir)
 
-	err = json.Unmarshal(testSpec, &testData)
-	require.NoError(t, err)
+	for _, testFile := range files {
+		testFile := testFile // Capture range variable
+		testName := strings.TrimSuffix(filepath.Base(testFile), ".json")
 
-	for i, c := range testData.TestCases {
-		t.Run(strconv.Itoa(i), func(t *testing.T) {
-			assert := assert.New(t)
-			actual := flagengine.GetEvaluationResult(&c.EvaluationContext)
-			expected := c.EvaluationResult
+		t.Run(testName, func(t *testing.T) {
+			t.Parallel()
 
-			assert.Equal(expected.Flags, actual.Flags)
+			// Read the test case file
+			testSpec, err := os.ReadFile(testFile)
+			require.NoError(t, err)
+			require.NotEmpty(t, testSpec)
+
+			// Parse the test case
+			var testCase struct {
+				Context engine_eval.EngineEvaluationContext `json:"context"`
+				Result  engine_eval.EvaluationResult        `json:"result"`
+			}
+
+			err = json.Unmarshal(testSpec, &testCase)
+			require.NoError(t, err)
+
+			// Run the evaluation
+			actual := flagengine.GetEvaluationResult(&testCase.Context)
+			expected := testCase.Result
+
+			// Compare the results
+			assert.Equal(t, expected.Flags, actual.Flags)
 		})
 	}
 }
